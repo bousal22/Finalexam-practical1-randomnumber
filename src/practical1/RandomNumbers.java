e77dae89cfca32d59a5ca4abeffb9b5cb3b967cb
bousal22 <boutihsalah@gmail.com>
2022-07-12 03:34:33 -0400

    final commit

--- src/practical1/RandomNumbers.java
+++ src/practical1/RandomNumbers.java
@@ -1,6 +1,7 @@
 package practical1;
 
-
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,28 +9,31 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomNumbers {
 
 	public static void main(String[] args) {
-		
-		 
 
-		
+		Scanner scanner = new Scanner(System.in);
+
 		Set<Integer> mySet = new TreeSet<>();
 
 		System.out.println(ThreadLocalRandom.current().nextInt());
-		mySet.add(ThreadLocalRandom.current().nextInt());
-		
-		while (mySet.size() != 500) {
-			mySet.add(ThreadLocalRandom.current().nextInt());
 
+		while (mySet.size() < 500) {
+			mySet.add(Math.abs(ThreadLocalRandom.current().nextInt()));
 		}
 		System.out.println(mySet);
 		System.out.println(mySet.size());
-		Object[] mySetArray = mySet.toArray();
-		System.out.println(mySetArray[0]);
-		System.out.println(mySetArray[3]);
+		Object[] mySetArray = mySet.toArray();		
 		
-	
-		
-		
+		System.out.println("give me the smallest number");
+		int number = scanner.nextInt();
+		int result = smallestNumber(mySet, number - 1);
+		System.out.println("the "+ number +" smallest number is:" + result);
+		scanner.close();
+	}
 
+	public static int smallestNumber(Set myset, int mynum) {
+		Object[] myarray = myset.toArray();
+		int mysmallest = (int) myarray[mynum];
+
+		return mysmallest;
 	}
 }
